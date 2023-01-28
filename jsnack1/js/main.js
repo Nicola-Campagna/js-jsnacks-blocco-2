1547bc8dfa1efd072d0b35abe27b646366ba0d17
Nicola-Campagna <campagnanicola379@gmail.com>
2023-01-28 19:27:58 +0100

    final commit jsnack1

--- jsnack1/js/main.js
+++ jsnack1/js/main.js
@@ -1,2 +1,13 @@
 // JSnack1
 // Inserisci un numero, se è pari stampa il numero, se è dispari stampa il numero successivo.
+
+let insertNumber = prompt("inserisci numero?");
+// console.log(insertNumber);
+
+if (insertNumber % 2 == 0) {
+    console.log(insertNumber);
+}
+else {
+    insertNumber++
+    console.log("numero succ." + insertNumber);
+}
