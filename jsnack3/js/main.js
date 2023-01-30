c42460d2a02ff1e675fbaf60a3e0f2774af86ca6
Nicola-Campagna <campagnanicola379@gmail.com>
2023-01-30 14:35:49 +0100

    final commit jsnack3

--- jsnack3/js/main.js
+++ jsnack3/js/main.js
@@ -1,2 +1,24 @@
 // JSnack3
-// Crea un array di numeri interi e fai la somma di tutti gli elementi  che sono in posizione dispari
+// Crea un array di numeri interi 
+// e fai la somma di tutti gli elementi  che sono in posizione dispari
+
+
+// Array n°interi
+const numbers = [
+    1, //indice 0 (pari)
+    3, //indice 1 (dispari)
+    5, //indice 2 (pari)
+    7, //indice 3 (dispari)
+    9, //indice 4 (pari)
+]
+let somma = 0;
+// ciclo+array
+for (let i = 0; i < numbers.length; i++) {
+    const currentIndex = numbers[i];
+    if (i % 2 != 0) {
+        somma = somma + currentIndex;
+        console.log("numero in posizione dispara: " + currentIndex);
+    }
+}
+
+
